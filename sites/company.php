b65147334a412705c12f2c308b1561be4c2b42b0
Matteo Scheitz Violi <matteosvm@gmail.com>
2017-01-05 12:10:59 +0100

    Changed company strike order, centered logo, added translations

--- sites/company.php
+++ sites/company.php
@@ -11,24 +11,31 @@
         <img src="../media/logos/companies/<?= $company->getNameCode() ?>.svg" alt="<?= $company->getNameCode() ?>" />
     </a>
 </div>
-
+<!--<?php
+    $guaranteed_routes='<?= _("guaranteed_routes") ?>';
+    $guaranteed_routes_url='<?= $strikes[$i]->getGuaranteedRoutesUrl() ?>';
+    echo'<div id="guarenteed-routes-div">
+        <p id="when-where-tag">'.guaranteed_routes.':</p>
+        <a>'.guaranteed_routes_url.'</a>
+    </div>';
+?>-->
 <?php for($i=0; $i<count($strikes); $i++): ?>
 <div class="strike-box">
-    <div id="content-div">
-        <p id="when-where-tag"><?= _("date") ?>:</p>
-        <p><?= $strikes[$i]->getStartDate() ?></p>
-        <p><?= $strikes[$i]->getEndDate() ?></p>
-    </div>
-    <div class="first-separator-div"></div>
     <div id="content-div">
         <p  id="when-where-tag"><?= _("region") ?>:</p>
         <p><?= $strikes[$i]->getRegion() ?></p>
     </div>
-    <div>
     <div class="first-separator-div"></div>
     <div id="content-div">
         <p  id="when-where-tag"><?= _("province") ?>:</p>
         <p><?= $strikes[$i]->getProvince() ?></p>
+    </div>
+    <div>
+    <div class="first-separator-div"></div>
+    <div id="content-div">
+        <p id="when-where-tag"><?= _("date") ?>:</p>
+        <p><?= $strikes[$i]->getStartDate() ?></p>
+        <p><?= $strikes[$i]->getEndDate() ?></p>
     </div>
     <div class="first-separator-div"></div>
     <div id="content-div">
