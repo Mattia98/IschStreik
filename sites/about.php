984be4b13b6094894bc41ee4752af26583f0fc20
matteomsv <matteosvm@gmail.com>
2017-01-11 14:29:24 +0100

    Added en_GB logo, changed about.php

--- sites/about.php
+++ sites/about.php
@@ -1,40 +1,46 @@
-<p>Developed by The IschStreik Team</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae tellus vitae velit pulvinar viverra. Suspendisse eleifend, nisi sit amet placerat facilisis, metus tortor scelerisque eros, iaculis scelerisque orci neque quis mauris. Nulla faucibus blandit lorem, viverra cursus ex porttitor non. Donec nec tellus at augue commodo fringilla. In nunc purus, pharetra et viverra sit amet, congue eget magna. Quisque ac ligula sit amet enim vulputate eleifend. Duis tempus purus in gravida lacinia. Nam condimentum a ex a lobortis. Nunc placerat sagittis luctus. Quisque placerat laoreet felis, in congue turpis aliquet a. Praesent varius eu orci vitae maximus.</p>
+<div class="about-div">
+    <h3>About us</h3>
+    <p>IschStreick - un govane team di teste creative. Il nostro team è composto di un webdesigner e tre programmatori.</p>
+    <p id="contact">Contatto:</p>
+    <a href="mailto:sciopero.news@gmail.com" >sciopero.news@gmail.com</a>
+    <p id="school">Scuola</p>
+    <p><a href="http://www.bozen.berufsschule.it/">Scuola professionale provinciale per l`industria e l`artigianato</a></p>
 
-<p>Donec dictum eros et malesuada gravida. Aliquam eu nisi convallis metus luctus suscipit vitae elementum ex. Mauris dictum pulvinar nisi. Quisque lobortis mauris ac neque tristique porta. Nullam vitae purus iaculis, viverra libero imperdiet, rutrum elit. Pellentesque eget gravida lectus, ut sagittis nulla. Nullam ante mi, laoreet at ultricies non, dictum non tortor. In tincidunt, tortor a mollis finibus, orci nisi semper augue, vel bibendum risus augue eu nisi. Donec varius imperdiet tortor, ac tincidunt libero. Ut tincidunt urna nec massa lacinia interdum in at libero. Nam lobortis arcu ut leo imperdiet, quis volutpat odio mattis.</p>
+    <details>
+        <summary>Used apps:</summary>
+        <figure>
+            <img src="../media/logos/about/bluefish.png" alt="BLUEFISH LOGO" />
+            <figcaption>Bluefish editor</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/apache2.png" alt="APACHE2 LOGO" />
+            <figcaption>APACHE 2</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/php.png" alt="PHP LOGO" />
+            <figcaption>PHP 7</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/postgresql.png" alt="PostgreSQL LOGO" />
+            <figcaption>PostgreSQL 9.5</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/linux.png" alt="LINUX LOGO" />
+            <figcaption>LINUX</figcaption>
+        </figure>
 
-<h4>Used apps:</h4>
-<figure>
-	<img src="../media/logos/about/bluefish.png" alt="BLUEFISH LOGO" />
-	<figcaption>Bluefish editor</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/apache2.png" alt="APACHE2 LOGO" />
-	<figcaption>APACHE 2</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/php.png" alt="PHP LOGO" />
-	<figcaption>PHP 7</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/postgresql.png" alt="PostgreSQL LOGO" />
-	<figcaption>PostgreSQL 9.5</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/linux.png" alt="LINUX LOGO" />
-	<figcaption>LINUX</figcaption>
-</figure>
-
-<h4>Used tecnologies:</h4>
-<figure>
-	<img src="../media/logos/about/html5.png" alt="HTML5 LOGO" />
-	<figcaption>HTML 5</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/css3.png" alt="CSS3 LOGO" />
-	<figcaption>CSS 3</figcaption>
-</figure>
-<figure>
-	<img src="../media/logos/about/js6.png" alt="ES6 LOGO" />
-	<figcaption>JS EcmaSript 6</figcaption>
-</figure>
+        <h4>Used tecnologies:</h4>
+        <figure>
+            <img src="../media/logos/about/html5.png" alt="HTML5 LOGO" />
+            <figcaption>HTML 5</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/css3.png" alt="CSS3 LOGO" />
+            <figcaption>CSS 3</figcaption>
+        </figure>
+        <figure>
+            <img src="../media/logos/about/js6.png" alt="ES6 LOGO" />
+            <figcaption>JS EcmaSript 6</figcaption>
+        </figure>
+    </details>
+</div>
