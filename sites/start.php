2c0e5f9fa3f7b79eeeb54e4e0786a04c57f57429
Mattia Dorigatti <io.mattia98@gmail.com>
2017-01-16 11:06:01 +0100

    Region search automatically searches

--- sites/start.php
+++ sites/start.php
@@ -17,7 +17,7 @@
 <div id="dropdown-menu">
 	<form method="GET" action="./" >
 		<label for="region"><?= _("region").":" ?></label>
-		<select  name="region">
+		<select  name="region" onchange="if(this.value != 0) { this.form.submit(); }">
 			<?php for($i=0; $i<count($regions); $i++): ?>
 			<option <?= ($region == $regions[$i]) ? "selected" : "" ?>><?= $regions[$i] ?></option>
 			<?php endfor; ?>
