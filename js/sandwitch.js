9de4d809028d9e2f69e9d1a9434c632dde97f0c0
Mattia Dorigatti <io.mattia98@gmail.com>
2017-02-06 12:26:50 +0100

    Improved popup behavioure

--- js/sandwitch.js
+++ js/sandwitch.js
@@ -5,18 +5,23 @@
 		$('#sandwitch').addEventListener('click', sandwitch);
 		$('#language-selector').addEventListener('click', languagePopup);
         $('#description-selector').addEventListener('click', descriptionPopup);
-		$('#curtain').addEventListener('click', curtain);
+		$('#curtain').addEventListener('click', closeAll);
 		$$('.logo').forEach((obj) => obj.addEventListener('error', removeLogo));
 		$$('.logo-cp').forEach((obj) => obj.addEventListener('error', replaceLogo));
 	};
 	
+	const closeAll = () => {
+		$('#language-popup').setAttribute('class', 'popup-closed');
+		$('#description-popup').setAttribute('class', 'popup-closed');
+		$('nav').setAttribute('class', 'nav-closed');
+		$('#curtain').setAttribute('class', 'curtain-open');
+		$('body').setAttribute('style', '');
+		$('main').setAttribute('style', '');
+	};
+
 	const sandwitch = () => {
-		if (isNavOpen()) {
-			$('nav').setAttribute('class', 'nav-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
-		} else {
+		closeAll();
+		if(!isNavOpen()) {
 			$('nav').setAttribute('class', 'nav-open');
 			$('#curtain').setAttribute('class', 'curtain-closed');
 			$('body').setAttribute('style', 'overflow:hidden');
@@ -25,12 +30,8 @@
 	};
 
 	const languagePopup = () => {
-		if (isLangPopupOpen()) {
-			$('#language-popup').setAttribute('class', 'popup-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
-		} else {
+		closeAll();
+		if (!isLangPopupOpen()) {
 			$('#language-popup').setAttribute('class', 'popup-open');
 			$('#curtain').setAttribute('class', 'curtain-closed');
 			$('body').setAttribute('style', 'overflow:hidden');
@@ -39,37 +40,12 @@
 	};
     
     const descriptionPopup = () => {
-		if (isDescPopupOpen()) {
-			$('#description-popup').setAttribute('class', 'popup-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
-		} else {
+		closeAll();
+		if (!isDescPopupOpen()) {
 			$('#description-popup').setAttribute('class', 'popup-open');
 			$('#curtain').setAttribute('class', 'curtain-closed');
 			$('body').setAttribute('style', 'overflow:hidden');
 			$('main').setAttribute('style', 'filter: blur(5px);');
-		}
-	};
-
-	const curtain = () => {
-		if (isLangPopupOpen()) {
-			$('#language-popup').setAttribute('class', 'popup-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
-		}
-        if (isDescPopupOpen()) {
-			$('#description-popup').setAttribute('class', 'popup-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
-		}
-		if (isNavOpen()) {
-			$('nav').setAttribute('class', 'nav-closed');
-			$('#curtain').setAttribute('class', 'curtain-open');
-			$('body').setAttribute('style', '');
-			$('main').setAttribute('style', '');
 		}
 	};
 
