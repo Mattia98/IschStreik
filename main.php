e60b941ed91bc89ef5278e233a9830ca3d485d73
matteomsv <matteosvm@gmail.com>
2017-02-20 12:27:18 +0100

    Updated translations

--- main.php
+++ main.php
@@ -73,7 +73,7 @@
 		<nav>
 
 		<ul class="list-up">
-            <a href="?site=settings"><li><i class="material-icons">settings</i>Settings</li></a>
+            <a href="?site=settings"><li><i class="material-icons">notifications</i>Notifications</li></a>
 			<a href="?site=stats"><li><i class="material-icons">timeline</i>Stats</li></a>
 			<li id="li-feedback"><i class="material-icons">feedback</i>Feedback<i class="material-icons arrow">arrow_drop_down</i></li>
             <ul id="list-feedback" class="popup-closed">
