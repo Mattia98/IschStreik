88d8f1f31d8d88ae7a74bbc4d8a3980b43c4ed4d
matteomsv <matteosvm@gmail.com>
2017-03-08 14:53:50 +0100

    Fixed div separator bug

--- sites/start.php
+++ sites/start.php
@@ -35,28 +35,27 @@
         </noscript>
     </form>
 </div>
-
 <div class="box-container">
-<?php if(isset($_GET["nojs"])): ?>
-<?php for($i=0; $i<count($companies); $i++): ?>
-    <?php
-        $colourClass = "green";
-        if(Strike::findBySomethingAmount($companies[$i]->getId(), "companyId")>0) {
-            $colourClass = "orange";
-            if($companies[$i]->getTimeToNextStrike()<2) {
-                $colourClass = "red";
-            }
-        }
-    ?>
-    <a href="?site=company&id=<?= $companies[$i]->getId() ?>&nojs=true">
-    <div class="box <?= $colourClass ?>">
-        <div class="imgBox">
-            <img class="logo" src="../media/logos/companies/<?= $companies[$i]->getNameCode() ?>.svg" alt="<?= $companies[$i]->getNameCode() ?>"/>
-        </div>
-        <span><?= $companies[$i]->getName() ?></span>
-    </div>
-    <div class="separator-div"></div>
-    </a>
-<?php endfor; ?>
-<?php endif; ?>
+    <?php if(isset($_GET["nojs"])): ?>
+        <?php for($i=0; $i<count($companies); $i++): ?>
+            <?php
+                $colourClass = "green";
+                if(Strike::findBySomethingAmount($companies[$i]->getId(), "companyId")>0) {
+                    $colourClass = "orange";
+                    if($companies[$i]->getTimeToNextStrike()<2) {
+                        $colourClass = "red";
+                    }
+                }
+            ?>
+            <a href="?site=company&id=<?= $companies[$i]->getId() ?>&nojs=true">
+            <div class="box <?= $colourClass ?>">
+                <div class="imgBox">
+                    <img class="logo" src="../media/logos/companies/<?= $companies[$i]->getNameCode() ?>.svg" alt="<?= $companies[$i]->getNameCode() ?>"/>
+                </div>
+                <span><?= $companies[$i]->getName() ?></span>
+                <div class="separator-div"></div>
+            </div>
+            </a>
+        <?php endfor; ?>
+    <?php endif; ?>
 </div>
