7d6d05eb93d539acb8fd468e89134bf332a8a959
Mattia98 <Mattia98@users.noreply.github.com>
2017-04-24 17:35:03 +0200

    Test new cookie consent

--- main.php
+++ main.php
@@ -70,11 +70,30 @@
 		<?php endif; ?>
 
 		<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+		<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+		<script>
+		window.addEventListener("load", function(){
+		window.cookieconsent.initialise({
+		  "palette": {
+		    "popup": {
+		      "background": "#333"
+		    },
+		    "button": {
+		      "background": "#a7c712"
+		    }
+		  },
+		  "theme": "edgeless",
+		  "position": "bottom-right"
+		})});
+		</script>
+		<!--
 		<script type="text/javascript">
 		    window.cookieconsent_options = {"message":"This site uses cookies. By continuing to browse the site, you are agreeing to our use of cookies.","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"dark-floating"};
 		</script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
-
+		-->
+		
 		<title><?= $GLOBALS["site-name"] ?> | <?= _("sitename") ?></title>
 	</head>
 	<body>
